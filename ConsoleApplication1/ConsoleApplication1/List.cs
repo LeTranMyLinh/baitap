9afda3c6110b9cfe03a4950a32174c43ee2f80a7
LeTranMyLinh <realmsf.it@gmail.com>
2016-09-26 09:11:21 +0700

    up List

---
+++ ConsoleApplication1/ConsoleApplication1/List.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class List<L>
+    {
+        Element<L> head;
+
+        internal Element<L> Head
+        {
+            get { return head; }
+            set { head = value; }
+        }
+        public List()
+        { }
+        public void addfirst (Element<L> e)
+        {
+            e.Next = head;
+            this.head = e;
+        }
+    }
+}
