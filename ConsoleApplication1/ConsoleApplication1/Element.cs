554365cd8ddf052e5858911c1baacaadef829db9
LeTranMyLinh <realmsf.it@gmail.com>
2016-09-26 08:23:04 +0700

    baitapC#

---
+++ ConsoleApplication1/ConsoleApplication1/Element.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Element<L>
+    {
+        private L data;
+
+        public L Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        Element<L> next;
+
+        internal Element<L> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+       
+    }
+}
